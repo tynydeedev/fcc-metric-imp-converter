dc7fa942383c2178cb8f7f333f7e2e24e9d09138
tynydeedev <88718743+tynydeedev@users.noreply.github.com>
2021-09-02 09:42:08 +0000

    create getNum function and test

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,7 +2,29 @@ function ConvertHandler() {
   
   this.getNum = function(input) {
     let result;
-    
+    // Separate the first number-like from the input
+    let number = input.split(/[a-z]/i).filter(e => e != '')[0];
+    // If there is no number-like input, make it 1
+    (!number) ? number = '1' : number;
+    // Only allow digit, dot and forward slash
+    const regex = /[^\d\.\/]/;
+    const test = regex.test(number);
+    if (test) throw new Error('invalid number');
+    // Check if there are forward slash
+    const slashCheck = /\//.test(number);
+    if (slashCheck) {
+      const numberOfSlash = number.match(/\//g).length;
+      if (numberOfSlash === 1) {
+        number = number.split('/').map(num => Number(num));
+        if (!number[0] || !number[1]) throw new Error('invalid number');
+        result = number[0] / number[1];
+      } else {
+        throw new Error('invalid number');
+      }
+    } else {
+      if (!Number(number)) throw new Error('invalid number');
+      result = Number(number);
+    }
     return result;
   };
   
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,25 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite('Unit Tests', function() {
+  this.timeout(500);
+  suite('Right number and input', function() {
+    test('get the initial number', function() {
+      assert.strictEqual(convertHandler.getNum('4km'), 4, 'should get the whole number');
+      assert.strictEqual(convertHandler.getNum('4km.'), 4, 'should get the whole number while invalid unit');
+      assert.strictEqual(convertHandler.getNum('4.2km'), 4.2, 'should get the float number');
+      assert.throws(function(){convertHandler.getNum('4..2km')}, Error, null, 'should not accept invalid number');
+      assert.throws(function(){convertHandler.getNum('?km')}, Error, null, 'should not accept invalid number');
+      assert.throws(function(){convertHandler.getNum('4..?2km')}, Error, null, 'should not accept invalid number');
+      assert.throws(function(){convertHandler.getNum('4,2km')}, Error, null, 'should not accept comma');
+      assert.strictEqual(convertHandler.getNum('4/5km'), 0.8, 'should get the fractional number');
+      assert.strictEqual(convertHandler.getNum('4.5/8km'), 0.5625, 'should get the fractional number with decimal');
+      assert.throws(function(){convertHandler.getNum('4,2/2km')}, Error, null, 'should not accept comma');
+      assert.strictEqual(convertHandler.getNum('4.2/2.1km'), 2, 'should get the fractional number with decimals');
+      assert.throws(function(){convertHandler.getNum('4.2/2/3km')}, Error, null, 'should not accept more than 1 slash');
+      assert.strictEqual(convertHandler.getNum('km'), 1, 'should return 1 if no input number');
+    });
+
+  })
 
 });
